744dfcb5f2d167e3cbf028ced70d21d9ccea7a9b
IgorAssuncao <igor.ormonde@gmail.com>
2019-12-09 23:48:18 +0000

    finished

--- at/script.js
+++ at/script.js
@@ -1,12 +1,9 @@
 app.inicio = function() {
-  const buttonStart = document.getElementById("buttonStart");
-  // buttonStart.onclick = () => startGame();
-
   app.firstTime
     ? initializeBoard()
-    : !app.gameStarted
-    ? (buttonStart.onclick = () => startGame())
-    : (buttonStart.onclick = () => stopGame());
+    : app.gameStarted
+    ? stopGame()
+    : startGame();
 };
 
 (function() {
@@ -23,6 +20,8 @@ app.inicio = function() {
   ];
 
   app.imagens = imagens;
+  app.moves = 0;
+  app.points = 0;
 
   const body = document.body;
   const buttonStart = document.createElement("button");
@@ -36,42 +35,293 @@ app.inicio = function() {
   buttonStart.setAttribute("class", "button");
   buttonStart.setAttribute("id", "buttonStart");
   buttonStart.innerHTML = "Iniciar jogo";
+  buttonStart.onclick = app.inicio;
   body.appendChild(buttonStart);
+
+  const moves = document.createElement("p");
+  moves.setAttribute("id", "moves");
+  moves.innerHTML = `Movimentos: ${app.moves} - Pontos: ${app.points}`;
+
+  body.appendChild(moves);
+
+  const mensagem = document.createElement("h2");
+  mensagem.setAttribute("id", "mensagem");
+  mensagem.style.display = "none";
+
+  body.appendChild(mensagem);
+
+  const timer = document.createElement("p");
+  timer.setAttribute("id", "timer");
+
+  body.appendChild(timer);
 
   tabuleiro.setAttribute("id", "tabuleiro");
 
   body.appendChild(tabuleiro);
+
+  localStorage.setItem("ranking", JSON.stringify({}));
+
+  const ranking = document.createElement("p");
+  ranking.setAttribute("id", "ranking");
+  ranking.style.display = "none";
+
+  body.appendChild(ranking);
 })();
 
-// function createStopButton() {
-//   if (!document.getElementById("buttonRestart")) {
-//     const body = document.body;
-//     const buttonRestart = document.createElement("button");
+function initializeBoard() {
+  app.moves = 0;
+  app.points = 0;
+  app.cardsOpen = [];
+  app.cardsMatched = [];
+  app.timer = 0;
 
-//     buttonRestart.setAttribute("class", "button");
-//     buttonRestart.setAttribute("id", "buttonRestart");
+  const body = document.body;
 
-//     buttonRestart.innerHTML = "Reinciar";
+  const tabuleiro = document.getElementById("tabuleiro");
 
-//     body.appendChild(buttonRestart);
-//   }
-// }
+  const mensagem = document.getElementById("mensagem");
+  mensagem.style.display = "none";
 
-function checkClickOnCard(event) {
-  return event.target.parentNode.id.split('/')[0] === 'img';
+  const timer = document.getElementById("timer");
+  timer.innerHTML = `Tempo: ${app.timer} s`;
+
+  body.appendChild(timer);
+
+  if (app.cards.length <= 0) {
+    app.imagens.forEach(img => {
+      if (img !== "img/cross.png") app.cards.push(img);
+    });
+
+    app.cards = sortCards(app.cards.concat(app.cards));
+
+    let index = 1;
+    app.cards.forEach(card => {
+      tabuleiro.appendChild(mountCardHtml(card, index));
+      index++;
+    });
+  }
+
+  unflipAllCards();
+
+  body.appendChild(tabuleiro);
+
+  app.firstTime = false;
 }
 
-function flipCard(event) {
-  const target = event.target.parentNode;
+function startGame() {
+  app.gameStarted = true;
 
-  if (checkClickOnCard(event)) {
-    target.childNodes[0].style.display = "block";
-    target.childNodes[1].style.display = "none";
+  const button = document.getElementById("buttonStart");
+  button.innerHTML = "Parar jogo";
+  button.onclick = app.inicio;
+
+  const allDivCards = document
+    .getElementById("tabuleiro")
+    .getElementsByClassName("divCard");
+
+  for (divCard of allDivCards) {
+    divCard.onclick = cardOpen;
+  }
+
+  flipAllCards();
+
+  setTimeout(() => {
+    if (app.gameStarted) unflipAllCards();
+  }, 3000);
+
+  app.timer = 0;
+
+  setInterval(() => {
+    if (app.points < 8) {
+      ++app.timer;
+      updateTimer();
+    }
+  }, 1000);
+}
+
+function stopGame() {
+  cleanUp();
+  initializeBoard();
+  app.gameStarted = false;
+  app.points = 0;
+  app.moves = 0;
+  app.cardsMatched = [];
+  app.cardsOpen = [];
+  app.timer = 0;
+}
+
+function cleanUp() {
+  const button = document.getElementById("buttonStart");
+  button.innerHTML = "Iniciar jogo";
+
+  document.getElementById("tabuleiro").innerHTML = "";
+
+  const rankingParapgraph = document.getElementById("ranking");
+  rankingParapgraph.innerHTML = "";
+  rankingParapgraph.style.display = "none";
+
+  app.cards = [];
+}
+
+function sortCards(cards) {
+  let current = cards.length,
+    randomIndex;
+
+  while (current !== 0) {
+    randomIndex = Math.floor(Math.random() * current);
+    current--;
+
+    [cards[current], cards[randomIndex]] = [cards[randomIndex], cards[current]];
+  }
+
+  return cards;
+}
+
+function cardOpen() {
+  flipCard(this);
+  app.cardsOpen.push(this);
+  app.moves++;
+  updateScore();
+
+  if (app.cardsOpen.length === 2) {
+    checkCardsMatch(app.cardsOpen[0], app.cardsOpen[1])
+      ? match(app.cardsOpen[0], app.cardsOpen[1])
+      : unmatch(app.cardsOpen[0], app.cardsOpen[1]);
+  }
+
+  if (app.points === 8) {
+    setTimeout(() => {
+      congratulations();
+    }, 3000);
   }
 }
 
-function unflipCard() {
+function match(card, otherCard) {
+  disableCards(card, otherCard);
+  app.cardsMatched.push(card);
+  app.cardsMatched.push(otherCard);
 
+  app.cardsOpen = [];
+
+  app.points++;
+  updateScore();
+}
+
+function unmatch(card, otherCard) {
+  setTimeout(() => {
+    unflipCard(card);
+    unflipCard(otherCard);
+  }, 1000);
+  app.cardsOpen = [];
+}
+
+function disableCards(card, otherCard) {
+  card.onclick = "";
+  otherCard.onclick = "";
+}
+
+function checkCardsMatch(card, otherCard) {
+  return card.id === otherCard.id && card.value !== otherCard.value;
+}
+
+function updateScore() {
+  const moves = document.getElementById("moves");
+  moves.innerHTML = `Movimentos: ${app.moves} - Pontos: ${app.points}`;
+}
+
+function updateTimer() {
+  const timer = document.getElementById("timer");
+  timer.innerHTML = `Tempo: ${app.timer} s`;
+}
+
+function mountCardHtml(card, index) {
+  const divCard = document.createElement("div");
+  divCard.setAttribute("class", "divCard");
+  divCard.setAttribute("id", `${card}`);
+  divCard.value = index;
+
+  const cardFront = document.createElement("img");
+  cardFront.setAttribute("class", "cardFront");
+  cardFront.setAttribute("src", `${card}`);
+
+  divCard.appendChild(cardFront);
+
+  const cardBack = document.createElement("img");
+  cardBack.setAttribute("class", "cardBack");
+  cardBack.setAttribute("src", "img/cross.png");
+
+  divCard.appendChild(cardBack);
+
+  return divCard;
+}
+function flipCard(card) {
+  card.childNodes[1].animate(
+    [
+      {
+        opacity: 1,
+        height: "100px"
+      },
+      {
+        opacity: 0,
+        height: 0
+      }
+    ],
+    {
+      duration: 500
+    }
+  );
+  card.childNodes[0].animate(
+    [
+      {
+        opacity: 0,
+        height: 0
+      },
+      {
+        opacity: 1,
+        height: "100px"
+      }
+    ],
+    {
+      duration: 500
+    }
+  );
+  card.childNodes[1].style.display = "none";
+  card.childNodes[0].style.display = "block";
+}
+
+function unflipCard(card) {
+  card.childNodes[0].animate(
+    [
+      {
+        opacity: 1,
+        height: "100px"
+      },
+      {
+        opacity: 0,
+        height: 0
+      }
+    ],
+    {
+      duration: 500
+    }
+  );
+  card.childNodes[1].animate(
+    [
+      {
+        opacity: 0,
+        height: 0
+      },
+      {
+        opacity: 1,
+        height: "100px"
+      }
+    ],
+    {
+      duration: 500
+    }
+  );
+  card.childNodes[0].style.display = "none";
+  card.childNodes[1].style.display = "block";
 }
 
 function flipAllCards() {
@@ -96,116 +346,42 @@ function unflipAllCards() {
   }
 }
 
-function checkCardsMatch(card, otherCard) {
-  return card === otherCard;
-}
+function congratulations() {
+  const ranking = JSON.parse(localStorage.getItem("ranking"));
+  ranking[app.timer] = {
+    movimentos: app.moves,
+    pontos: app.points
+  };
 
-function initializeBoard() {
-  const body = document.body;
-  const tabuleiro = document.getElementById("tabuleiro");
-
-  if (app.cards.length <= 0) {
-    app.imagens.forEach(img => {
-      if (img !== "img/cross.png") app.cards.push(img);
-    });
-
-    app.cards = sortCards(app.cards).concat(sortCards(app.cards));
-  }
-
-  app.cards.forEach(card => {
-    tabuleiro.appendChild(mountCardHtml(card));
-  });
-
-  unflipAllCards();
-
-  body.appendChild(tabuleiro);
-
-  app.firstTime = false;
-  app.inicio();
-}
-
-function startGame() {
-  app.gameStarted = true;
-
-  const button = document.getElementById("buttonStart");
-  button.innerHTML = "Parar jogo";
-
-  const cardsMatched = [];
-  const cardsPair = [];
-  
-  const allDivCards = document.getElementsByClassName("divCard");
-  
-  for (divCard of allDivCards) {
-    divCard.onclick = flipCard;
-  }
-
-  flipAllCards();
-
-  setTimeout(() => { 
-    if (app.gameStarted) unflipAllCards();
-  }, 3000);
-  
-  // createStopButton();
-
-  if (app.gameStarted) {
-    console.log(checkClickOnCard(event))
-    if (checkClickOnCard(event)) {
-      console.log('entrou aqui oh')
-      cardsPair.push(event.target);
-      console.log(cardsPair);
-      if (cardsPair.length == 2) checkCardsMatch(cardsPair[0], cardsPair[1]);
-
-      app.gameStarted = !checkAllCardsMatch();
-    }
-    // break;
-  }
-}
-
-function stopGame() {
-  cleanUp();
-  initializeBoard();
+  localStorage.setItem("ranking", JSON.stringify(ranking));
   app.gameStarted = false;
-}
+  cleanUp();
 
-function restartGame() {
-  stopGame();
-  startGame();
-}
-
-function sortCards(cards) {
-  cards.sort(() => {
-    return 0.3 - Math.random();
-  });
-
-  return cards;
-}
-
-function mountCardHtml(card) {
-  const divCard = document.createElement("div");
-  divCard.setAttribute("class", "divCard");
-  divCard.setAttribute("id", `${card}`);
-
-  const cardFront = document.createElement("img");
-  cardFront.setAttribute("class", "cardFront");
-  cardFront.setAttribute("src", `${card}`);
-
-  divCard.appendChild(cardFront);
-
-  const cardBack = document.createElement("img");
-  cardBack.setAttribute("class", "cardBack");
-  cardBack.setAttribute("src", "img/cross.png");
-
-  divCard.appendChild(cardBack);
-
-  return divCard;
-}
-
-function cleanUp() {
   const button = document.getElementById("buttonStart");
+  button.innerHTML = "Jogar novamente";
 
-  document.getElementById("tabuleiro").innerHTML = '';
+  button.onclick = () => {
+    cleanUp();
+    initializeBoard();
+    startGame();
+  };
 
-  button.innerHTML = "Iniciar jogo";
+  const mensagem = document.createElement("mensagem");
+  mensagem.setAttribute("id", "mensagem");
+  mensagem.innerHTML = "Parabens, voce venceu";
+  mensagem.style.display = "block";
+
+  document.getElementById("tabuleiro").appendChild(mensagem);
+
+  const body = document.body;
+  const rankingParapgraph = document.getElementById("ranking");
+  rankingParapgraph.style.display = "block";
+  rankingParapgraph.innerHTML = "<br>Ranking:<br>";
+
+  for (element in ranking) {
+    rankingParapgraph.innerHTML += `${element} - Movimentos: ${ranking[element].movimentos} s - Pontos: ${ranking[element].pontos}<br>`;
+  }
+  body.appendChild(rankingParapgraph);
 }
 
 app.inicio();
