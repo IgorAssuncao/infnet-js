f61f824184884fc1dada1cc295e454a19358ca56
IgorAssuncao <igor.ormonde@gmail.com>
2019-12-08 22:37:44 -0300

    wip restart game

--- at/script.js
+++ at/script.js
@@ -1,4 +1,10 @@
-app.inicio = function() {};
+app.inicio = function() {
+  app.firstTime
+    ? initializeBoard()
+    : !app.gameStarted
+    ? startGame()
+    : restartGame();
+};
 
 (function() {
   var imagens = [
@@ -13,12 +19,15 @@ app.inicio = function() {};
     "img/cross.png"
   ];
 
+  app.imagens = imagens;
+
   const body = document.body;
   const button = document.createElement("button");
   const tabuleiro = document.createElement("div");
 
   app.cards = [];
 
+  app.firstTime = true;
   app.gameStarted = false;
 
   button.setAttribute("class", "button");
@@ -28,24 +37,58 @@ app.inicio = function() {};
 
   tabuleiro.setAttribute("id", "tabuleiro");
 
-  body.appendChild(tabuleiro);
-
   button.onclick = () => app.inicio();
+
+  body.appendChild(tabuleiro);
 })();
 
-function startGame() {
-  imagens.forEach(img => {
-    if (img !== "img/cross.png") cards.push(img);
+function initializeBoard() {
+  const body = document.body;
+  const tabuleiro = document.getElementById("tabuleiro");
+
+  app.imagens.forEach(img => {
+    if (img !== "img/cross.png") app.cards.push(img);
   });
 
-  cards = sortCards(cards).concat(sortCards(cards));
+  app.cards = sortCards(app.cards).concat(sortCards(app.cards));
 
-  cards.forEach(card => {
+  app.cards.forEach(card => {
     tabuleiro.appendChild(mountCardHtml(card));
   });
+
+  for (element of tabuleiro.getElementsByClassName("cardFront")) {
+    element.style.display = "none";
+  }
+
+  body.appendChild(tabuleiro);
+
+  app.firstTime = false;
 }
 
-function restartGame() {}
+function startGame() {
+  app.gameStarted = true;
+  const body = document.body;
+  const button = document.getElementById("button");
+  const tabuleiro = document.getElementById("tabuleiro");
+
+  for (element of tabuleiro.getElementsByClassName("cardBack")) {
+    element.style.display = "none";
+  }
+  for (element of tabuleiro.getElementsByClassName("cardFront")) {
+    element.style.display = "block";
+  }
+
+  setTimeout(() => {}, 3000);
+
+  button.innerHTML = "Reiniciar jogo";
+}
+
+function restartGame() {
+  cleanUp();
+  console.log(document.getElementById("tabuleiro"));
+  initializeBoard();
+  app.gameStarted = false;
+}
 
 function sortCards(cards) {
   cards.sort(() => {
@@ -71,8 +114,21 @@ function mountCardHtml(card) {
 
   divCard.appendChild(cardBack);
 
-  console.log(divCard);
   return divCard;
+}
+
+function cleanUp() {
+  const body = document.body;
+  const button = document.getElementById("button");
+
+  body.removeChild(document.getElementById("tabuleiro"));
+
+  const tabuleiro = document.createElement("div");
+  tabuleiro.setAttribute("id", "tabuleiro");
+  console.log(tabuleiro);
+
+  button.innerHTML = "Iniciar jogo";
+  body.appendChild(tabuleiro);
 }
 
 app.inicio();
